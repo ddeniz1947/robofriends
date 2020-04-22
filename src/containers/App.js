6faa16717f82c3f7709e17ac48369d921a0007cf
Deniz Can DİNÇ <ddeniz1947@gmail.com>
2020-04-22 18:15:11 +0300

    redux starter

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,7 +3,7 @@ import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import './App.css';
 import Scroll from '../components/Scroll'
-
+import ErrorBoundry from '../components/ErrorBoundry'
 
 class App extends Component {
     constructor(){
@@ -42,7 +42,9 @@ class App extends Component {
                 <h1 className='f1'>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
                 <Scroll>
-                <CardList robots = {filterRobots}/>
+                    <ErrorBoundry>
+                    <CardList robots = {filterRobots}/>
+                    </ErrorBoundry>
                 </Scroll>
                 
                 </div>
